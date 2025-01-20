984d3b0dc6450434507a23023ac3f4062763d2cf
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2025-01-20 21:35:54 +0100

    Project code skeleton initialized
    Project structure, as well as main dependencies has been added.

---
+++ OncoCodersWebApp/OncoCodersWebApp/src/main/java/org/example/oncocoderswebapp/OncoCodersWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.oncocoderswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncoCodersWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncoCodersWebAppApplication.class, args);
+    }
+
+}
---
+++ OncoCodersWebApp/OncoCodersWebApp/src/test/java/org/example/oncocoderswebapp/OncoCodersWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oncocoderswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncoCodersWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
