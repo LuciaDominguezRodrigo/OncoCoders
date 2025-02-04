8bf803a6c1ded1a366de82c53b53c8ffc469e533
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-02-04 13:11:09 +0100

    feat: Create Admin User Sections

---
+++ ProjectFrontend/src/app/sections/admin-user/ia-model-configuration/ia-model-configuration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ia-model-configuration',
+  imports: [],
+  templateUrl: './ia-model-configuration.component.html',
+  styleUrl: './ia-model-configuration.component.css'
+})
+export class IAModelConfigurationComponent {
+
+}
---
+++ ProjectFrontend/src/app/sections/admin-user/not-banned-users/not-banned-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-banned-users',
+  imports: [],
+  templateUrl: './not-banned-users.component.html',
+  styleUrl: './not-banned-users.component.css'
+})
+export class NotBannedUsersComponent {
+
+}
