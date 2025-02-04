af7e45541539e350166ccef7ce9350c81ed735fc
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-02-04 13:12:23 +0100

    feat: Create Pacient User Sections

---
+++ ProjectFrontend/src/app/sections/pacient-user/diagnosis/diagnosis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnosis',
+  imports: [],
+  templateUrl: './diagnosis.component.html',
+  styleUrl: './diagnosis.component.css'
+})
+export class DiagnosisComponent {
+
+}
