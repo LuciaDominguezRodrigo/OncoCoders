1ffad1332bfa36e6990dc0f21dd51ae5e0f84f09
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-02-18 22:51:44 +0100

    feat: layout unbanned users

--- ProjectFrontend/src/app/sections/admin-user/not-banned-users/not-banned-users.component.ts
+++ ProjectFrontend/src/app/sections/admin-user/not-banned-users/not-banned-users.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { SectiontitleComponent } from "../../../components/tags/sectiontitle/sectiontitle.component";
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-not-banned-users',
-  imports: [],
+  imports: [SectiontitleComponent, CommonModule],
   templateUrl: './not-banned-users.component.html',
-  styleUrl: './not-banned-users.component.css'
 })
 export class NotBannedUsersComponent {
+  users: any[] = [{ name: "Paco Malaco", role: "MEDICUSER"}];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void { // TODO Get All Users
+    this.userService.getPatients().subscribe(
+      (data) => {
+        //this.users = data;
+        //console.log(data);
+      },
+      (error) => { console.error('Error al obtener pacientes', error); }
+    );
+  }
+
+  //TODO Ban Action
 }
