d2a7f4b9bd4170fedd78da9332116bbb710071fb
Lucía Domínguez Rodrigo <117301166+LuciaDominguezRodrigo@users.noreply.github.com>
2025-02-19 23:24:12 +0100

    feat: changes remade for

--- ProjectBackend/src/main/java/org/example/oncocoderswebapp/repository/DiagnosisRepository.java
+++ ProjectBackend/src/main/java/org/example/oncocoderswebapp/repository/DiagnosisRepository.java
@@ -1,9 +1,13 @@
 package org.example.oncocoderswebapp.repository;
 
+import org.example.oncocoderswebapp.model.User;
 import org.example.oncocoderswebapp.model.UserDiagnosis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DiagnosisRepository extends JpaRepository<UserDiagnosis,Long> {
+    List<UserDiagnosis> findByUsuarioOrderByIdAsc(User user);
 }
